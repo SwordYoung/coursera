6b9e40b746e8f3a64f0ac32d98eb1ae3d78880c4
Jian <jian@atrenta.com>
2014-09-21 18:27:28 -0700

    rr

--- algorithm_part1/week1/Percolation/Percolation.java
+++ algorithm_part1/week1/Percolation/Percolation.java
@@ -55,29 +55,31 @@ public class Percolation {
            } else {
                if (r0[0] == r1[0] && r0[1] == r1[1]) {
                } else {
-                   int size0 = mSize[i0][j0];
-                   int size1 = mSize[i1][j1];
-                   if (size0 < size1) {
-                       mParents_i[i0][j0] = r1[0];
-                       mParents_j[i0][j0] = r1[1];
-                       
-//                       mParents_i[r0[0]][r0[1]] = r1[0];
-//                       mParents_j[r0[0]][r0[1]] = r1[1];
-                   } else if (size0 > size1) {
-                       mParents_i[i1][j1] = r0[0];
-                       mParents_j[i1][j1] = r0[1];
+                   mParents_i[i0][j0] = r1[0];
+                   mParents_j[i0][j0] = r1[1];
+//                   int size0 = mSize[i0][j0];
+//                   int size1 = mSize[i1][j1];
+//                   if (size0 < size1) {
+//                       mParents_i[i0][j0] = r1[0];
+//                       mParents_j[i0][j0] = r1[1];
 //                       
-//                       mParents_i[r1[0]][r1[1]] = r0[0];
-//                       mParents_j[r1[0]][r1[1]] = r0[1];
-                   } else {
-                       mParents_i[i1][j1] = r0[0];
-                       mParents_j[i1][j1] = r0[1];
-                       
-//                       mParents_i[r1[0]][r1[1]] = r0[0];
-//                       mParents_j[r1[0]][r1[1]] = r0[1];
-                       
-                       mSize[r0[0]][r0[1]] += 1;
-                   }
+////                       mParents_i[r0[0]][r0[1]] = r1[0];
+////                       mParents_j[r0[0]][r0[1]] = r1[1];
+//                   } else if (size0 > size1) {
+//                       mParents_i[i1][j1] = r0[0];
+//                       mParents_j[i1][j1] = r0[1];
+////                       
+////                       mParents_i[r1[0]][r1[1]] = r0[0];
+////                       mParents_j[r1[0]][r1[1]] = r0[1];
+//                   } else {
+//                       mParents_i[i1][j1] = r0[0];
+//                       mParents_j[i1][j1] = r0[1];
+//                       
+////                       mParents_i[r1[0]][r1[1]] = r0[0];
+////                       mParents_j[r1[0]][r1[1]] = r0[1];
+//                       
+//                       mSize[r0[0]][r0[1]] += 1;
+//                   }
                }
            }
        }
